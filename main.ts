bc1b09509ee2fafc7fb4602b7fb4b0735f61d5f7
SpoildMilk7757 <101339945+SpoildMilk7757@users.noreply.github.com>
2022-05-17 09:54:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,3 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 8) {
-        Presses = 0
-    }
-})
-input.onButtonPressed(Button.A, function () {
-    Presses += 1
-    if (Presses == 1) {
-        radio.sendNumber(7)
-        basic.pause(1000)
-    } else {
-        radio.sendNumber(8)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    Presses += 0 - Presses
-})
-let Presses = 0
 basic.showLeds(`
     . . . . .
     . # # # .
@@ -24,5 +6,9 @@ basic.showLeds(`
     . . . . .
     `)
 radio.setGroup(177)
-let Ambulance = 0
-Presses = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(7)
+        basic.pause(5000)
+    }
+})
