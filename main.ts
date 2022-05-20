a62920ea09bb538fc1c349c69dd58f87ee7a6b67
SpoildMilk7757 <101339945+SpoildMilk7757@users.noreply.github.com>
2022-05-20 08:42:38 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(7)
+    basic.pause(15000)
+})
 basic.showLeds(`
     . . . . .
     . # # # .
@@ -8,6 +12,9 @@ basic.showLeds(`
 radio.setGroup(177)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        radio.sendNumber(7)
+        for (let index = 0; index < 15; index++) {
+            music.playTone(831, music.beat(BeatFraction.Half))
+            music.playTone(659, music.beat(BeatFraction.Half))
+        }
     }
 })
